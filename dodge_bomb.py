1506dfa1941c1e77179afa4c4cfe9c455ce6a655
otsuka <c0a2526641@edu.teu.ac.jp>
2026-04-21 14:46:45 +0900

    画面内外判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,16 +4,29 @@ import random
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1100, 650
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 DELTA = {
     pg.K_UP: (0, -5), #上
     pg.K_DOWN: (0, 5), #下
     pg.K_LEFT: (-5, 0), #左
     pg.K_RIGHT: (5, 0), #右
 }
+
+
+WIDTH, HEIGHT = 1100, 650
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect 
+    戻り値：横方向，縦方向判定結果（True：画面内、False：画面外） 
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right: #横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: #縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -26,20 +39,19 @@ def main():
 
     bb_img = pg.Surface((20,20)) #爆弾用の空のSurfaceを作る
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #爆弾円を描く
-    bb_img.set_colorkey((0, 0, 0)) #
+    bb_img.set_colorkey((0, 0, 0)) 
     bb_rct = bb_img.get_rect() #爆弾Rectを取得する
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT) #爆弾の初期座標を設定する
     vx, vy = +5, +5
-
-
     clock = pg.time.Clock()
     tmr = 0
+
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
@@ -47,10 +59,19 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx , vy )
-        screen.blit(bb_img, bb_rct)
+        if check_bound(kk_rct) != (True, True): 
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx , vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        
+        screen.blit(bb_img, bb_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
